9d74bc3e0d3238840530c0817846c33330586bd3
Violonnn <mstevenviolon@gmail.com>
2024-10-08 20:59:46 -0700

    code update

--- src/tasklistingsystem/Tasklistingsystem.java
+++ src/tasklistingsystem/Tasklistingsystem.java
@@ -1,61 +1,104 @@
 package tasklistingsystem;
 import java.util.Scanner;
 public class Tasklistingsystem {
-    
-        public void addEmpAcc(){
+   
+    public void addEmpAcc(){
         Scanner sc = new Scanner(System.in);
         config conf = new config();
-        System.out.print("Account ID: ");
+        System.out.print("Acount ID: ");
         int accID = sc.nextInt();
         System.out.print("Employee First Name: ");
         String fname = sc.next();
-        System.out.println("Employee Last Name: ");
+        System.out.print("Employee Last Name: ");
         String lname = sc.next();
         System.out.print("Employee Email: ");
         String email = sc.next();
-        System.out.println("Employee Department");
+        System.out.print("Employee Department: ");
         String edpt = sc.next();
         System.out.print("Employee Role: ");
         String erole = sc.next();
 
-        String sql = "INSERT INTO employee-acc (acc_id, e_fname, e_lname, e_email, e_department, e_role) VALUES (?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO employee_acc (acc_id, e_fname, e_lname, e_email, e_department, e_role) VALUES (?, ?, ?, ?, ?, ?)";
 
-
-        conf.addAcc(sql, fname, lname, email, edpt, erole);
-
-
+        conf.addRecord(sql, accID, fname, lname, email, edpt, erole);
     }
+    
+     public void updateEmpTask(){
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter Account ID to Update: ");
+         int id = sc.nextInt();
+         
+         System.out.print("Enter new Department: ");
+         String edpt = sc.next();
+         System.out.print("Enter new Role: ");
+         String erole = sc.next();
+         
+        String sql = "UPDATE employee_acc SET e_department = ?, e_role = ? WHERE acc_id = ?";
+        
+        config conf = new config();
+        conf.updateRecord(sql, edpt, erole, id);
+    }
+     
+    public void deleteEmpTask(){
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter Account ID to Delete: ");
+         int id = sc.nextInt();
+         
+         String sql = "DELETE FROM employee_task WHERE acc_id = ?";
+        
+        config conf = new config();
+        conf.updateRecord(sql, id);
+    }
+     
     public static void main(String[] args) {
-        Tasklistingsystem tls = new Tasklistingsystem ();
+        Tasklistingsystem tls = new Tasklistingsystem();
         config conf = new config();
         Scanner sc = new Scanner (System.in);
-        
-        System.out.println("------Task Listing System----");
+        int chose;
+do{ 
+        System.out.println("------Welcome To Task Listing System----");
         System.out.println("1. Add Employee Acc");
-        System.out.println("2. Add Employee Task");
-        System.out.println("3. View Employee Task");
-        System.out.println("4. View Employee Acc");
-        System.out.println("5. Update Empployee Task");
-        System.out.println("6. Delete Employee Task");
-        System.out.println("7. Exit");
-        
+        System.out.println("2. View Employee Account");
+        System.out.println("3. Update Empployee Account");
+        System.out.println("4. Delete Employee Task");
+        System.out.println("5. Exit");
         System.out.print("Enter Action: ");
         int act = sc.nextInt();
-        
+       
         switch(act){
             case 1:
                 tls.addEmpAcc();
             break;
-            
+
             case 2:
-                break;
-            
-            case 3:
-                break;
-            
-            case 4:
+                System.out.println("\nList of Employee's Account");
                 conf.viewEmpAcc();
                 break;
+
+            case 3:
+                System.out.println("\nList of Employee's Account");
+                conf.viewEmpAcc();
+                tls.updateEmpTask();
+                break;
+
+            case 4:
+                System.out.println("\nList of Employee's Account");
+                conf.viewEmpAcc();
+                tls.deleteEmpTask();
+                break;
+
+            case 5:
+                System.out.println("Exiting...");
+                break;
+
+            default:
+                System.out.println("Action Error!");
         }
+        System.out.print("Enter Again: ");
+        chose = sc.nextInt();
+        
+}while(chose != 0);
+        System.out.println("Thank You!");
     }
+
 }
--- src/tasklistingsystem/config.java
+++ src/tasklistingsystem/config.java
@@ -10,39 +10,57 @@ public class config {
 public static Connection connectDB() {
         Connection con = null;
         try {
-            Class.forName("org.sqlite.JDBC"); 
-            con = DriverManager.getConnection("jdbc:sqlite:tls.db"); 
+            Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection("jdbc:sqlite:tls.db");
             System.out.println("Connection Successful");
         } catch (Exception e) {
             System.out.println("Connection Failed: " + e);
         }
         return con;
     }
-        public void addAcc(String sql, String... values) {
-        try (Connection conn = this.connectDB(); 
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+ public void addRecord(String sql, Object... values) {
+    try (Connection conn = this.connectDB(); // Use the connectDB method
+         PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            for (int i = 0; i < values.length; i++) {
-                pstmt.setString(i + 1, values[i]); 
+        // Loop through the values and set them in the prepared statement dynamically
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] instanceof Integer) {
+                pstmt.setInt(i + 1, (Integer) values[i]); // If the value is Integer
+            } else if (values[i] instanceof Double) {
+                pstmt.setDouble(i + 1, (Double) values[i]); // If the value is Double
+            } else if (values[i] instanceof Float) {
+                pstmt.setFloat(i + 1, (Float) values[i]); // If the value is Float
+            } else if (values[i] instanceof Long) {
+                pstmt.setLong(i + 1, (Long) values[i]); // If the value is Long
+            } else if (values[i] instanceof Boolean) {
+                pstmt.setBoolean(i + 1, (Boolean) values[i]); // If the value is Boolean
+            } else if (values[i] instanceof java.util.Date) {
+                pstmt.setDate(i + 1, new java.sql.Date(((java.util.Date) values[i]).getTime())); // If the value is Date
+            } else if (values[i] instanceof java.sql.Date) {
+                pstmt.setDate(i + 1, (java.sql.Date) values[i]); // If it's already a SQL Date
+            } else if (values[i] instanceof java.sql.Timestamp) {
+                pstmt.setTimestamp(i + 1, (java.sql.Timestamp) values[i]); // If the value is Timestamp
+            } else {
+                pstmt.setString(i + 1, values[i].toString()); // Default to String for other types
             }
-
-            pstmt.executeUpdate();
-            System.out.println("Record added successfully!");
-        } catch (SQLException e) {
-            System.out.println("Error adding record: " + e.getMessage());
         }
+
+        pstmt.executeUpdate();
+        System.out.println("Record added successfully!");
+    } catch (SQLException e) {
+        System.out.println("Error adding record: " + e.getMessage());
     }
-        
+}
         public void viewEmpAcc() {
-        String sql = "SELECT * FROM employee-acc";
+        String sql = "SELECT * FROM employee_acc";
         config conf = new config();
         try (Connection conn = conf.connectDB(); // Use the connectDB method
              PreparedStatement pstmt = conn.prepareStatement(sql);
              ResultSet rs = pstmt.executeQuery()) {
 
-            System.out.println("--------------------------------------------------------------------------------");
-            System.out.printf("| %-5s | %-20s | %-12s | %-25s | %-10s | %-10s |\n", "Account ID", "Firstname", "LastNAme", "Email", "Department", "Role");
-            System.out.println("--------------------------------------------------------------------------------");
+            System.out.println("-------------------------------------------------------------------------------------------------------");
+            System.out.printf("| %-5s | %-12s | %-12s | %-20s | %-10s | %-19s |\n", "Account ID", "Firstname", "LastNAme", "Email", "Department", "Role");
+            System.out.println("-------------------------------------------------------------------------------------------------------");
 
             while (rs.next()) {
                 int id = rs.getInt("acc_id");
@@ -50,13 +68,67 @@ public static Connection connectDB() {
                 String lname = rs.getString("e_lname");
                 String email = rs.getString("e_email");
                 String dpt = rs.getString("e_department");
-                String role = rs.getString("e_department");
-                System.out.printf("| %-5d | %-20s | %-12s | %-25s | %-10s | %-10s|\n", id, fname, lname, email, dpt, role);
+                String role = rs.getString("e_role");
+                System.out.printf("| %-10d | %-12s | %-12s | %-20s | %-10s | %-20s|\n", id, fname, lname, email, dpt, role);
             }
 
-            System.out.println("--------------------------------------------------------------------------------");
+            System.out.println("-------------------------------------------------------------------------------------------------------");
         } catch (SQLException e) {
-            System.out.println("Error retrieving citizens: " + e.getMessage());
+            System.out.println("Error retrieving employee account: " + e.getMessage());
         }
     }
+        
+        public void updateRecord(String sql, Object... values) {
+        try (Connection conn = this.connectDB(); // Use the connectDB method
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // Loop through the values and set them in the prepared statement dynamically
+            for (int i = 0; i < values.length; i++) {
+                if (values[i] instanceof Integer) {
+                    pstmt.setInt(i + 1, (Integer) values[i]); // If the value is Integer
+                } else if (values[i] instanceof Double) {
+                    pstmt.setDouble(i + 1, (Double) values[i]); // If the value is Double
+                } else if (values[i] instanceof Float) {
+                    pstmt.setFloat(i + 1, (Float) values[i]); // If the value is Float
+                } else if (values[i] instanceof Long) {
+                    pstmt.setLong(i + 1, (Long) values[i]); // If the value is Long
+                } else if (values[i] instanceof Boolean) {
+                    pstmt.setBoolean(i + 1, (Boolean) values[i]); // If the value is Boolean
+                } else if (values[i] instanceof java.util.Date) {
+                    pstmt.setDate(i + 1, new java.sql.Date(((java.util.Date) values[i]).getTime())); // If the value is Date
+                } else if (values[i] instanceof java.sql.Date) {
+                    pstmt.setDate(i + 1, (java.sql.Date) values[i]); // If it's already a SQL Date
+                } else if (values[i] instanceof java.sql.Timestamp) {
+                    pstmt.setTimestamp(i + 1, (java.sql.Timestamp) values[i]); // If the value is Timestamp
+                } else {
+                    pstmt.setString(i + 1, values[i].toString()); // Default to String for other types
+                }
+            }
+
+            pstmt.executeUpdate();
+            System.out.println("Record updated successfully!");
+        } catch (SQLException e) {
+            System.out.println("Error updating record: " + e.getMessage());
+        }
+    }
+        
+        public void deleteRecord(String sql, Object... values) {
+        try (Connection conn = this.connectDB();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // Loop through the values and set them in the prepared statement dynamically
+            for (int i = 0; i < values.length; i++) {
+                if (values[i] instanceof Integer) {
+                    pstmt.setInt(i + 1, (Integer) values[i]); // If the value is Integer
+                } else {
+                    pstmt.setString(i + 1, values[i].toString()); // Default to String for other types
+                }
+            }
+
+            pstmt.executeUpdate();
+            System.out.println("Record deleted successfully!");
+        } catch (SQLException e) {
+            System.out.println("Error deleting record: " + e.getMessage());
+            }
+        }   
 }
